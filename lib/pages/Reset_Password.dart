37a0c935241c72dbabe5c881755102fac90c9f53
mustafa ahmed <91737288+Arthurdotself@users.noreply.github.com>
2024-03-29 01:20:10 +0300

    the login page fixed , the reset password logic fixed

--- lib/pages/Reset_Password.dart
+++ lib/pages/Reset_Password.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:tugas1_login/pages/login.dart';
 
 class Reset_Password extends StatefulWidget {
   const Reset_Password({Key? key}) : super(key: key);
@@ -11,22 +12,14 @@ class Reset_Password extends StatefulWidget {
 
 class _Reset_PasswordState extends State<Reset_Password> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  late String email ;
+  late String email;
   late String msg = "";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'PMS',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
@@ -46,9 +39,9 @@ class _Reset_PasswordState extends State<Reset_Password> {
               ),
               Container(
                 padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-                child: const Text('Enter the Email associated with your account for which you forgot your password.'),
+                child: const Text(
+                    'Enter the Email associated with your account for which you forgot your password.'),
               ),
-
               Container(
                 padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
                 child: TextField(
@@ -63,36 +56,49 @@ class _Reset_PasswordState extends State<Reset_Password> {
                   ),
                 ),
               ),
-
               Container(
-                  height: 80,
-                  padding: const EdgeInsets.all(20),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size.fromHeight(50),
-                    ),
-                    child: const Text('Send Verification Code'),
-                    onPressed: () async {Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const VeCode()),
-                    );
+                height: 80,
+                padding: const EdgeInsets.all(20),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size.fromHeight(50),
+                  ),
+                  child: const Text('send a reset password to your email'),
+                  onPressed: () async {
                     try {
-                      if(email.isEmpty){
-                        msg="Enter your email";
-                      }else if(email.isNotEmpty){
-                        msg='No Account found for that email.';
+                      if (email.isEmpty) {
+                        msg = "Enter your email";
+                      } else if (email.isNotEmpty) {
+                        msg = 'No Account found for that email.';
                       }
                       var user = await _auth.sendPasswordResetEmail(email: email);
-                    } catch (Error)
-                    {AwesomeDialog(
+                      // Show success message using AwesomeDialog
+                      AwesomeDialog(
+                        context: context,
+                        dialogType: DialogType.success,
+                        animType: AnimType.bottomSlide,
+                        title: 'Success',
+                        desc: 'Password reset email sent successfully.\nCheck your email.',
+                        btnOkOnPress: () {
+                          // Navigate to another page
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Login()),
+                          );
+                        },
+                      ).show();
+                    } catch (error) {
+                      // Show error message using AwesomeDialog
+                      AwesomeDialog(
                         context: context,
                         dialogType: DialogType.error,
                         animType: AnimType.rightSlide,
                         title: 'Error',
-                        desc: msg ,
-                    ).show();}
-                    },
-                  )
+                        desc: msg,
+                      ).show();
+                    }
+                  },
+                ),
               ),
             ],
           ),
@@ -101,155 +107,3 @@ class _Reset_PasswordState extends State<Reset_Password> {
     );
   }
 }
-
-class VeCode extends StatelessWidget {
-  const VeCode({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'PMS',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Password Reset'),
-        ),
-        body: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                padding: const EdgeInsets.fromLTRB(20, 20, 20, 70),
-                child: const FlutterLogo(
-                  size: 40,
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-                child: const Text('Enter the verification code we have sent you on your Email.'),
-              ),
-
-              Container(
-                padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(90.0),
-                    ),
-                    labelText: 'Verification Code',
-                  ),
-                ),
-              ),
-
-              Container(
-                  height: 80,
-                  padding: const EdgeInsets.all(20),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size.fromHeight(50),
-                    ),
-                    child: const Text('Verify'),
-                    onPressed: () {Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const NewPassword()),
-                    );
-                      },
-                  )),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-class NewPassword extends StatelessWidget {
-  const NewPassword({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'PMS',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Password Reset'),
-        ),
-        body: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                padding: const EdgeInsets.fromLTRB(20, 20, 20, 70),
-                child: const FlutterLogo(
-                  size: 40,
-                ),
-              ),
-
-
-              Container(
-                padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(90.0),
-                    ),
-                    labelText: 'New Password',
-                  ),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(90.0),
-                    ),
-                    labelText: 'Re-Enter Password',
-                  ),
-                ),
-              ),
-
-              Container(
-                  height: 80,
-                  padding: const EdgeInsets.all(20),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size.fromHeight(50),
-                    ),
-                    child: const Text('Supmit'),
-                    onPressed: () {},
-                  )),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
